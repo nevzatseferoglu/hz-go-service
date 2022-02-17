0f807bd0d3581d97be29f1b65b48b9526d6f9b14
Nevzat Seferoglu <nevzat.seferoglu@hazelcast.com>
2022-02-17 11:19:13 +0300

    add single entry GET and PORT request handler

--- hz/factory.go
+++ hz/factory.go
@@ -2,52 +2,17 @@ package hz
 
 import (
 	"context"
-	"log"
+	"fmt"
 
 	"github.com/hazelcast/hazelcast-go-client"
 	"github.com/hazelcast/hazelcast-go-client/logger"
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 )
 
+const withoutKubernetes = true
+
 type ClientInfo struct {
-	Name    string `json:"name"`
-	Running bool   `json:"running"`
-}
-
-func getHazelcastClusterIP() (string, error) {
-	// getting config for the client inside kuberneetes cluster
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		return "", err
-	}
-	// creates clientset for given config
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return "", err
-	}
-	// return cluster services resources
-	hzService, err := clientset.CoreV1().Services("default").Get(context.TODO(), "hazelcast", metav1.GetOptions{})
-	if errors.IsNotFound(err) {
-		log.Printf("Service hazelcast not found in default namespace\n")
-		return "", err
-	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-		log.Printf("Error getting service %v\n", statusError.ErrStatus.Message)
-		return "", err
-	} else if err != nil {
-		return "", err
-	} else {
-		log.Printf("Found hazelcast service in default namespace\n")
-	}
-
-	return hzService.Spec.ClusterIP, nil
-	// cluster address
-	// base, err := getHazelcastClusterIP()
-	// if err != nil {
-	//	return nil, err
-	// }
+	ClientName    string `json:"clientName"`
+	ClientRunning bool   `json:"clientRunning"`
 }
 
 func NewHzClient(ctx context.Context) (*hazelcast.Client, error) {
@@ -55,7 +20,11 @@ func NewHzClient(ctx context.Context) (*hazelcast.Client, error) {
 		ClientName: "hz-go-service-client",
 	}
 	cc := &config.Cluster
-	cc.Network.SetAddresses("hazelcast.default.svc" + ":5701")
+	if true {
+		cc.Network.SetAddresses(fmt.Sprintf("%s:%s", "localhost", "5701"))
+	} else {
+		cc.Network.SetAddresses(fmt.Sprintf("%s:%s", "hazelcast.default.svc", "5701"))
+	}
 	cc.Discovery.UsePublicIP = false
 	cc.Unisocket = true
 	config.Logger.Level = logger.InfoLevel
--- service.go
+++ service.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -13,19 +15,27 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/hazelcast/hazelcast-go-client"
+	"github.com/hazelcast/hazelcast-go-client/types"
 
 	"github.com/nevzatseferoglu/hz-go-service/hz"
 )
 
+const hztimeout = time.Second * 3
+const sampleMap = "myDistributedMap"
+
 var (
 	config    *ServiceConfig
-	storage   *InMemoryHzStorage
+	imdg      *InMemoryHzStorage
+	hzc       *hazelcast.Client
 	endpoints = [...]string{"config", "map"}
 )
 
+type Info struct {
+	Msg string `json:"msg"`
+}
+
 type InMemoryHzStorage struct {
-	hzClient *hazelcast.Client
-	myMap    *hazelcast.Map
+	myMap *hazelcast.Map
 }
 
 type ServiceConfig struct {
@@ -37,47 +47,168 @@ type ServiceConfig struct {
 func getConfigHandler(rw http.ResponseWriter, _ *http.Request) {
 	rw.Header().Add("Content-Type", "application/json")
 	rw.WriteHeader(http.StatusOK)
-
-	hzClient := storage.hzClient
-
 	serviceConfigResponse := struct {
 		ServiceConfig
 		hz.ClientInfo
-	}{*config, hz.ClientInfo{Name: hzClient.Name(), Running: hzClient.Running()}}
-
+	}{*config, hz.ClientInfo{ClientName: hzc.Name(), ClientRunning: hzc.Running()}}
 	rspJson, err := json.Marshal(serviceConfigResponse)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	_, err = rw.Write(rspJson)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func getMapHandler(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Add("Content-Type", "application/json")
-	rw.WriteHeader(http.StatusOK)
+func validateURLQueryParameter(r *http.Request) (mapNames, keys []string, err error) {
+	// resolve query parameters
+	if err := r.ParseForm(); err != nil {
+		return nil, nil, err
+	}
+	var ok bool
+	if mapNames, ok = r.Form["name"]; !ok {
+		return nil, nil, errors.New("invalid request body")
+	}
+	if keys, ok = r.Form["key"]; !ok {
+		return nil, nil, errors.New("invalid request body")
+	}
+	return mapNames, keys, nil
+}
 
-	entries, err := storage.myMap.GetEntrySet(r.Context())
+func mapEntryHandler(rw http.ResponseWriter, r *http.Request) {
+	// set response type
+	rw.Header().Set("Content-Type", "application/json")
+	var err error
+	mapNames, keys, err := validateURLQueryParameter(r)
 	if err != nil {
-		http.Error(rw, "error on get map", http.StatusInternalServerError)
+		http.Error(rw, fmt.Sprintf("URL query parameters cannot be validated, err: %v.", err), http.StatusBadRequest)
+		return
 	}
-	log.Println("My distributed map entries are: ")
-	for _, entry := range entries {
-		log.Printf("key: %v, value: %v", entry.Key, entry.Value)
+	mapName := mapNames[0]
+	key := keys[0]
+	// wrap a timeout context for the hazelcast operation
+	ctx, cancel := context.WithTimeout(r.Context(), hztimeout)
+	defer cancel()
+	objInfo, err := hzc.GetDistributedObjectsInfo(ctx)
+	if err != nil {
+		http.Error(rw, "Hazelcast objects info cannot be obtained", http.StatusInternalServerError)
+		return
 	}
+	exist := false
+	for _, o := range objInfo {
+		if hazelcast.ServiceNameMap == o.ServiceName && mapName == o.Name {
+			exist = true
+			break
+		}
+	}
+	switch r.Method {
+	case "GET":
+		rw.WriteHeader(http.StatusOK)
+		// return proper response as a json value
+		var rsp []byte
+		if !exist {
+			nonExistMap := Info{
+				Msg: "There is no map for the given name",
+			}
+			rsp, err = json.Marshal(nonExistMap)
+			if err != nil {
+				http.Error(rw, fmt.Sprintf("marshal cannot work properly for non exist map response, rsp: %v\n", nonExistMap), http.StatusInternalServerError)
+				return
+			}
+			_, err = rw.Write(rsp)
+			return
+		}
+		// get map
+		m, err := hzc.GetMap(ctx, mapName)
+		if err != nil {
+			http.Error(rw, fmt.Sprintf("map: %s cannot be returned from the hazelcast", mapName), http.StatusInternalServerError)
+			return
+		}
+		// get value
+		value, err := m.Get(ctx, key)
+		if err != nil {
+			http.Error(rw, fmt.Sprintf("Value of key: %s cannot be returned from the hazelcast", key), http.StatusInternalServerError)
+			return
+		}
+		if value == nil {
+			nonExist := Info{
+				Msg: "There is no value for the given key",
+			}
+			rsp, err = json.Marshal(nonExist)
+			if err != nil {
+				http.Error(rw, fmt.Sprintf("marshal cannot work properly for non exist value response, rsp: %v\n", nonExist), http.StatusInternalServerError)
+				return
+			}
+		} else {
+			entry := types.Entry{
+				Key:   key,
+				Value: value,
+			}
+			rsp, err = json.Marshal(entry)
+			if err != nil {
+				http.Error(rw, fmt.Sprintf("marshal cannot work properly for the existing value response, rsp: %v\n", entry), http.StatusInternalServerError)
+				return
+			}
+		}
+		_, err = rw.Write(rsp)
+		if err != nil {
+			http.Error(rw, fmt.Sprintf("response cannot be properly written\n"), http.StatusInternalServerError)
+			return
+		}
+	case "POST":
+		rw.WriteHeader(http.StatusCreated)
+		if !isContentTypeJson(r) {
+			http.Error(rw, "Invalid content type is given in header.\n", http.StatusBadRequest)
+			return
+		}
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "response body cannot be read\n", http.StatusInternalServerError)
+			return
+		}
+		type RequestBody struct {
+			Value interface{} `json:"value"`
+		}
+		var reqBody RequestBody
+		err = json.Unmarshal(body, &reqBody)
+		if err != nil {
+			http.Error(rw, "Invalid request body", http.StatusBadRequest)
+			return
+		}
+		var rsp []byte
+		pmap, err := hzc.GetMap(ctx, mapName)
+		if err != nil {
+			http.Error(rw, "Hazelcast cannot return a map\n", http.StatusInternalServerError)
+			return
+		}
+		_, err = pmap.Put(ctx, key, reqBody.Value)
+		if err != nil {
+			http.Error(rw, fmt.Sprintf("Hazelcast cannot put %v value to map\n", reqBody.Value), http.StatusInternalServerError)
+			return
+		}
+		successInfo := Info{
+			Msg: fmt.Sprintf("(Key: %s, Value: %v) has been put to Map: %s successfully",
+				key, reqBody.Value, mapName),
+		}
+		rsp, err = json.Marshal(successInfo)
+		if err != nil {
+			http.Error(rw, fmt.Sprintf("marshal cannot work properly for non exist map put, rsp: %v\n", successInfo), http.StatusInternalServerError)
+			return
+		}
+		_, err = rw.Write(rsp)
+	}
+}
 
-	//rspJson, err := json.Marshal(storage.myMap)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//_, err = rw.Write(rspJson)
-	//if err != nil {
-	//	log.Fatalf("Problem with writing!, %v\n", err)
-	//}
+func isContentTypeJson(r *http.Request) bool {
+	contentType := r.Header.Get("Content-type")
+	if contentType == "" {
+		return false
+	}
+	if contentType != "application/json" {
+		return false
+	}
+	return true
 }
 
 func healthHandler(rw http.ResponseWriter, _ *http.Request) {
@@ -88,26 +219,27 @@ func readinessHandler(rw http.ResponseWriter, _ *http.Request) {
 	rw.WriteHeader(http.StatusOK)
 }
 
-func newDefaultConfig() *ServiceConfig {
+func newDefaultServiceConfig() *ServiceConfig {
 	// default config properties
-	c := &ServiceConfig{
+	return &ServiceConfig{
 		ServiceName: "hz-go-service",
 		Port:        8080,
-		Timeout:     10 * time.Second,
+		Timeout:     5 * time.Second,
 	}
-	return c
 }
 
-func newInMemoryHzStorage(ctx context.Context) (*InMemoryHzStorage, error) {
-	newClient, err := hz.NewHzClient(ctx)
+func newInMemoryHzStorage(ctx context.Context, c *hazelcast.Client) (*InMemoryHzStorage, error) {
+	entries := []types.Entry{
+		{Key: "key1", Value: "value1"},
+		{Key: "key2", Value: "value2"},
+		{Key: "key3", Value: "value3"},
+	}
+	newMap, err := c.GetMap(ctx, sampleMap)
+	err = newMap.PutAll(ctx, entries...)
 	if err != nil {
 		return nil, err
 	}
-	newMap, err := newClient.GetMap(ctx, "myDistributedMap")
-	if err != nil {
-		return nil, err
-	}
-	return &InMemoryHzStorage{hzClient: newClient, myMap: newMap}, nil
+	return &InMemoryHzStorage{myMap: newMap}, nil
 }
 
 func newServer(router *mux.Router, config *ServiceConfig) *http.Server {
@@ -119,57 +251,66 @@ func newServer(router *mux.Router, config *ServiceConfig) *http.Server {
 	}
 }
 
-func main() {
-	// default context
-	ctx := context.TODO()
-	var err error
-	// set service config as default
-	config = newDefaultConfig()
-	// create new client instance
-	storage, err = newInMemoryHzStorage(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// put current map
-	_, err = storage.myMap.Put(ctx, "John", 1)
-	if err != nil {
-		log.Fatal(err)
-	}
+func newRouter() *mux.Router {
 	// creates a server
 	router := mux.NewRouter()
 	// health checks for kubernetes
 	router.HandleFunc("/health", healthHandler)
 	router.HandleFunc("/readiness", readinessHandler)
 	// service configuration handler
-	router.HandleFunc(fmt.Sprintf("/%s/get", endpoints[0]), getConfigHandler)
-	// hazelcast map handler
-	router.HandleFunc(fmt.Sprintf("/%s/get", endpoints[1]), getMapHandler)
+	router.HandleFunc(fmt.Sprintf("/%s", endpoints[0]), getConfigHandler)
+	// hazelcast map handlers
+	router.HandleFunc(fmt.Sprintf("/%s", endpoints[1]), mapEntryHandler)
+	return router
+}
+
+func main() {
+	ctx := context.Background()
+	var err error
+	// set service config as default
+	config = newDefaultServiceConfig()
+	// initiate a new client
+	hzc, err = hz.NewHzClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// create a new in-memory storage
+	imdg, err = newInMemoryHzStorage(ctx, hzc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	router := newRouter()
 	// create server instance
 	srv := newServer(router, config)
 	// start http server
 	go func() {
-		log.Println("Starting server...")
+		log.Println("Server is listening...")
 		log.Fatal(srv.ListenAndServe())
 	}()
 	// handle potential incoming errors
-	handleSignal(srv)
+	handleSignal(ctx, srv)
 }
 
-func handleSignal(srv *http.Server) {
+func handleSignal(ctx context.Context, srv *http.Server) {
+	// signal channel
 	signalChan := make(chan os.Signal, 1)
+	// add interrupts signals to signal channel
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-
 	// block until receiving signals
 	<-signalChan
-
+	// new context for limit the closing time
 	ctx, cancel := context.WithTimeout(context.Background(), config.Timeout)
 	defer cancel()
+	if hzc != nil {
+		err := hzc.Shutdown(ctx)
+		if err != nil {
+			log.Fatalf("Client cannot be shut down after the signal is received, err: %v\n", err)
+		}
+	}
 	err := srv.Shutdown(ctx)
 	if err != nil {
-		log.Fatalf("Server cannot be properly shut down! err: %v\n", err)
+		log.Fatalf("Server cannot be properly shut down after the signal is received, err: %v\n", err)
 	}
-
 	log.Println("Server has been shut down!")
 	os.Exit(0)
 }
